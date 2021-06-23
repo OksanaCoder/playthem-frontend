d0fdeaa17acc2c83c2b2aca41130bbac9b741553
OksanaCoder <oksishukh@yahoo.com>
2021-06-24 01:54:57 +0300

    first commit

--- src/components/Buttons/StartBtn/StartBtn.jsx
+++ src/components/Buttons/StartBtn/StartBtn.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import StyledStartBtn from './StyledComponent';
 
 const StartBtn = ({ width, background }) => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push('https://nexo-front-constructor.herokuapp.com/#/login');
+  };
   return (
-    <StyledStartBtn background={background} w={width}>
+    <StyledStartBtn background={background} w={width} onClick={handleClick}>
       Get Started Now
     </StyledStartBtn>
   );
